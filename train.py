90cc12cc42e57472b678e1405b9829989a817c88
Madankh <khadkasuman0000@gmail.com>
2024-03-07 10:08:47 +0545

    2th

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import torch.nn as nn;
 import torchtext.datasets as datasets
 from torch.utils.data import Dataset, DataLoader, random_split
 from torch.optim.lr_scheduler import LambdaLR
+from dataset import BillingualDataset, causal_mask
+from model import build_transformer
 
 # Huggingface datasets and tokenizers
 from datasets import load_dataset
@@ -31,3 +33,38 @@ def get_or_build_tokenizer(config, ds, lang):
     else:
         tokenizer = Tokenizer.from_file(str(tokenizer_path))
     return tokenizer
+
+def get_ds(config):
+    ds_raw = load_dataset('opus_books' , f'{config["lang_src"]} - {config['lang_tgt']}' , split='train')
+
+    # Build tokenizers
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw , config['lang_src'])
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw , config['lang_tgt'])
+
+    # Keep 90% for training and 10% for validation
+    train_ds_size = int(0.9 * len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw , val_ds_raw = random_split(ds_raw , [train_ds_size , val_ds_size])
+
+    train_ds = BillingualDataset(train_ds_raw , tokenizer_src , tokenizer_tgt , config['lang_src'] , config['lang_tgt'] , config['seq_len'])
+    val_ds = BillingualDataset(val_ds_raw , tokenizer_src , tokenizer_tgt , config['lang_src'] , config['lang_tgt'] , config['seq_len'])
+
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        max_len_src = max(max_len_src , len(src_ids))
+        max_len_tgt = max(max_len_tgt , len(tgt_ids))
+    print('Max length of source sentence {max_len_src}')
+    print('Max length of target sentence {max_len_tgt}')   
+
+    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'] , shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=1 , shuffle=True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+
+def get_model(config, vocab_src_len, vocal_tgt_len):
+    model = build_transformer(vocab_src_len , vocal_tgt_len , config['seq_len'] , config['seq_len'] , config['d_model'])
+    return model
